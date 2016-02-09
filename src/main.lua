f56fe25925a13687bdb9b1da10b3a78f04a03bff
Harrm <harrm@mail.ru>
2016-02-09 15:49:27 +0300

    Fix image proportions

--- src/main.lua
+++ src/main.lua
@@ -1,15 +1,19 @@
-MOAISim.openWindow('Test', 480, 320)
+MOAISim.openWindow('Test', 800, 600)
 
 viewport = MOAIViewport.new()
-viewport:setSize(480, 320)
-viewport:setScale(480, 320)
+viewport:setSize(800, 600)
+viewport:setScale(800, 600)
 
 layer = MOAILayer2D.new()
 layer:setViewport(viewport)
 
+texture = MOAITexture.new()
+texture:load('../data/geass.jpg')
+w, h = texture:getSize()
+k = w / h
 image = MOAIGfxQuad2D.new()
-image:setTexture('../data/geass.jpg')
-image:setRect(-100, -50, 100, 50)
+image:setTexture(texture)
+image:setRect(-120*k, -120, 120*k, 120)
 
 prop = MOAIProp2D.new()
 prop:setDeck(image)
@@ -21,7 +25,7 @@ font = MOAIFont.new()
 font:loadFromTTF('../data/allods_west.ttf')
 
 text = MOAITextLabel.new()
-text:setLoc(0, -50)
+text:setLoc(0, -100)
 text:setString("Coming soon...")
 text:setFont(font)
 text:setTextSize(32)
