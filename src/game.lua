fcdb9b470c28cd17e39efea79c1582d724bc78c3
Harrm <harrm@mail.ru>
2016-02-17 20:21:50 +0300

    Fixes in GUI

--- src/game.lua
+++ src/game.lua
@@ -21,15 +21,15 @@ function Game.init()
 	local Label = require("gui.label")
 	local Image = require("gui.image")
 	local Button = require("gui.button")
-
+	--[[
 	MOAIDebugLines.setStyle ( MOAIDebugLines.PARTITION_CELLS, 2, 1, 1, 1 )
 	MOAIDebugLines.setStyle ( MOAIDebugLines.PARTITION_PADDED_CELLS, 1, 0.5, 0.5, 0.5 )
 	MOAIDebugLines.setStyle ( MOAIDebugLines.PROP_WORLD_BOUNDS, 2, 0.75, 0.75, 0.75 )
 	MOAIDebugLines.setStyle ( MOAIDebugLines.PROP_MODEL_BOUNDS, 2, 1, 1, 1 )
 	GUI.getLayer():showDebugLines(true)
-
+	--]]
 	ResourceDefinitions.set("geass", {	type = ResourceDefinitions.Type.Image,
-										fileName = "geass.jpg",
+										fileName = "geass.png",
 										width = 1000/2,
 										height = 625/2
 								 	 })
@@ -50,27 +50,28 @@ function Game.init()
 										  fontSize = 26,
 										  dpi = 160
 								 	     })
-	local background = Image("geass")
-	background:setLoc(0, 0)
-	background:show()
+	MOAIUntzSystem.initialize()
+	Game.soundtrack = ResourceManager:get("soundtrack")
 
 	local headerStyle = MOAITextStyle.new()
 	headerStyle:setColor(1, 0, 0)
 	headerStyle:setFont(ResourceManager:get("header font"))
 	headerStyle:setSize(52)
-	
+
 	Game.header = Label(headerStyle)
 	Game.header:setText("RISE")
 	Game.header:setLoc(0, 150)
 	Game.header:show()
-	
+
+	local background = Image("geass")
+
 	Game.button = Button(headerStyle)
-	Game.button:setText("RISE")
 	Game.button:setTexture(Button.State.Normal, background)
 	Game.button:setTexture(Button.State.Hovered, background)
 	Game.button:setTexture(Button.State.Pressed, background)
-	Game.button:setCallback(function() print 'clicked' end)
-	Game.button:setLoc(0, 100)
+	Game.button:setCallback(function() Game.playOrStopMusic() end)
+	Game.button:setLoc(0, 0)
+	Game.button:show()
 
 	local style = MOAITextStyle.new()
 	style:setColor(0.4, 0, 0.9)
@@ -82,15 +83,20 @@ function Game.init()
 	Game.text:setLoc(0, -280)
 	Game.text:show()
 	
-	--MOAIUntzSystem.initialize()
-	
-	--Game.soundtrack = ResourceManager:get("soundtrack")
-	--Game.soundtrack:play()
-
 	Game.isOver = false
 
 	MOAIInputMgr.device.keyboard:setCallback(Game.processKeyboard)
 	io.flush()
+end
+
+
+
+function Game.playOrStopMusic()
+	if Game.soundtrack:isPlaying() then
+		Game.soundtrack:pause()
+	else
+		Game.soundtrack:play()
+	end
 end
 
 
